89445f62daf675a714b012d01a47ba422e379405
Brandon Jackson <brandonljackson23@gmail.com>
2020-06-24 22:51:38 -0600

    add header, footer and about section

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const inquirer = require('inquirer');
 const promptUser = () => {
   return inquirer.prompt([
@@ -134,5 +127,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+});
